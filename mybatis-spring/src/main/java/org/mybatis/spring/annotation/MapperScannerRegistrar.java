8f1ebba07723c04714b3cd268e13078753f59fcf
huangfusuper <huangfusuper@163.com>
2020-09-15 09:13:18 +0800

    初始化代码

---
+++ mybatis-spring/src/main/java/org/mybatis/spring/annotation/MapperScannerRegistrar.java
@@ -0,0 +1,179 @@
+/**
+ * Copyright 2010-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.mybatis.spring.annotation;
+
+import java.lang.annotation.Annotation;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.mybatis.spring.mapper.ClassPathMapperScanner;
+import org.mybatis.spring.mapper.MapperFactoryBean;
+import org.mybatis.spring.mapper.MapperScannerConfigurer;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.BeanNameGenerator;
+import org.springframework.context.ResourceLoaderAware;
+import org.springframework.context.annotation.ImportBeanDefinitionRegistrar;
+import org.springframework.core.annotation.AnnotationAttributes;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.type.AnnotationMetadata;
+import org.springframework.util.ClassUtils;
+import org.springframework.util.StringUtils;
+
+/**
+ * A {@link ImportBeanDefinitionRegistrar} to allow annotation configuration of MyBatis mapper scanning. Using
+ * an @Enable annotation allows beans to be registered via @Component configuration, whereas implementing
+ * {@code BeanDefinitionRegistryPostProcessor} will work for XML configuration.
+ *
+ * @author Michael Lanyon
+ * @author Eduardo Macarron
+ * @author Putthiphong Boonphong
+ *
+ * @see MapperFactoryBean
+ * @see ClassPathMapperScanner
+ * @since 1.2.0
+ */
+public class MapperScannerRegistrar implements ImportBeanDefinitionRegistrar, ResourceLoaderAware {
+
+  /**
+   * {@inheritDoc}
+   *
+   * @deprecated Since 2.0.2, this method not used never.
+   */
+  @Override
+  @Deprecated
+  public void setResourceLoader(ResourceLoader resourceLoader) {
+    // NOP
+  }
+
+  /**
+   * Spring回调的时候会回调这个方法
+   * @param importingClassMetadata 导入类的原信息
+   * @param registry 注册工具
+   */
+  @Override
+  public void registerBeanDefinitions(AnnotationMetadata importingClassMetadata, BeanDefinitionRegistry registry) {
+    //获取对应类 MapperScan 注解的全部属性信息
+    AnnotationAttributes mapperScanAttrs = AnnotationAttributes .fromMap(importingClassMetadata.getAnnotationAttributes(MapperScan.class.getName()));
+    if (mapperScanAttrs != null) {
+      //调用具体的实现
+      registerBeanDefinitions(importingClassMetadata, mapperScanAttrs, registry, generateBaseBeanName(importingClassMetadata, 0));
+    }
+  }
+
+  /**
+   * 注册一个 BeanDefinition  ，这里会构建并且向容器中注册一个bd 也就是一个自定义的扫描器 MapperScannerConfigurer
+   * @param annoMeta 被@Importd的类的原信息
+   * @param annoAttrs 注解的元信息，内部包含所有的注解属性
+   * @param registry Spring提供的注册到容器的工具类
+   * @param beanName bean的名称
+   */
+  void registerBeanDefinitions(AnnotationMetadata annoMeta, AnnotationAttributes annoAttrs,
+      BeanDefinitionRegistry registry, String beanName) {
+
+    BeanDefinitionBuilder builder = BeanDefinitionBuilder.genericBeanDefinition(MapperScannerConfigurer.class);
+    builder.addPropertyValue("processPropertyPlaceHolders", true);
+
+    Class<? extends Annotation> annotationClass = annoAttrs.getClass("annotationClass");
+    if (!Annotation.class.equals(annotationClass)) {
+      builder.addPropertyValue("annotationClass", annotationClass);
+    }
+
+    Class<?> markerInterface = annoAttrs.getClass("markerInterface");
+    if (!Class.class.equals(markerInterface)) {
+      builder.addPropertyValue("markerInterface", markerInterface);
+    }
+
+    Class<? extends BeanNameGenerator> generatorClass = annoAttrs.getClass("nameGenerator");
+    if (!BeanNameGenerator.class.equals(generatorClass)) {
+      builder.addPropertyValue("nameGenerator", BeanUtils.instantiateClass(generatorClass));
+    }
+
+    Class<? extends MapperFactoryBean> mapperFactoryBeanClass = annoAttrs.getClass("factoryBean");
+    if (!MapperFactoryBean.class.equals(mapperFactoryBeanClass)) {
+      builder.addPropertyValue("mapperFactoryBeanClass", mapperFactoryBeanClass);
+    }
+
+    String sqlSessionTemplateRef = annoAttrs.getString("sqlSessionTemplateRef");
+    if (StringUtils.hasText(sqlSessionTemplateRef)) {
+      builder.addPropertyValue("sqlSessionTemplateBeanName", annoAttrs.getString("sqlSessionTemplateRef"));
+    }
+
+    String sqlSessionFactoryRef = annoAttrs.getString("sqlSessionFactoryRef");
+    if (StringUtils.hasText(sqlSessionFactoryRef)) {
+      builder.addPropertyValue("sqlSessionFactoryBeanName", annoAttrs.getString("sqlSessionFactoryRef"));
+    }
+
+    List<String> basePackages = new ArrayList<>();
+    basePackages.addAll(
+        Arrays.stream(annoAttrs.getStringArray("value")).filter(StringUtils::hasText).collect(Collectors.toList()));
+
+    basePackages.addAll(Arrays.stream(annoAttrs.getStringArray("basePackages")).filter(StringUtils::hasText)
+        .collect(Collectors.toList()));
+
+    basePackages.addAll(Arrays.stream(annoAttrs.getClassArray("basePackageClasses")).map(ClassUtils::getPackageName)
+        .collect(Collectors.toList()));
+
+    if (basePackages.isEmpty()) {
+      basePackages.add(getDefaultBasePackage(annoMeta));
+    }
+
+    String lazyInitialization = annoAttrs.getString("lazyInitialization");
+    if (StringUtils.hasText(lazyInitialization)) {
+      builder.addPropertyValue("lazyInitialization", lazyInitialization);
+    }
+
+    builder.addPropertyValue("basePackage", StringUtils.collectionToCommaDelimitedString(basePackages));
+
+    registry.registerBeanDefinition(beanName, builder.getBeanDefinition());
+
+  }
+
+  private static String generateBaseBeanName(AnnotationMetadata importingClassMetadata, int index) {
+    return importingClassMetadata.getClassName() + "#" + MapperScannerRegistrar.class.getSimpleName() + "#" + index;
+  }
+
+  private static String getDefaultBasePackage(AnnotationMetadata importingClassMetadata) {
+    return ClassUtils.getPackageName(importingClassMetadata.getClassName());
+  }
+
+  /**
+   * A {@link MapperScannerRegistrar} for {@link MapperScans}.
+   *
+   * @since 2.0.0
+   */
+  static class RepeatingRegistrar extends MapperScannerRegistrar {
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void registerBeanDefinitions(AnnotationMetadata importingClassMetadata, BeanDefinitionRegistry registry) {
+      AnnotationAttributes mapperScansAttrs = AnnotationAttributes
+          .fromMap(importingClassMetadata.getAnnotationAttributes(MapperScans.class.getName()));
+      if (mapperScansAttrs != null) {
+        AnnotationAttributes[] annotations = mapperScansAttrs.getAnnotationArray("value");
+        for (int i = 0; i < annotations.length; i++) {
+          registerBeanDefinitions(importingClassMetadata, annotations[i], registry,
+              generateBaseBeanName(importingClassMetadata, i));
+        }
+      }
+    }
+  }
+
+}
---
+++ mybatis-spring/src/main/java/org/mybatis/spring/mapper/ClassPathMapperScanner.java
@@ -0,0 +1,271 @@
+/**
+ * Copyright 2010-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.mybatis.spring.mapper;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.logging.Logger;
+import org.mybatis.logging.LoggerFactory;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.AnnotatedBeanDefinition;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.BeanDefinitionHolder;
+import org.springframework.beans.factory.config.RuntimeBeanReference;
+import org.springframework.beans.factory.support.AbstractBeanDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.GenericBeanDefinition;
+import org.springframework.context.annotation.ClassPathBeanDefinitionScanner;
+import org.springframework.core.type.filter.AnnotationTypeFilter;
+import org.springframework.core.type.filter.AssignableTypeFilter;
+import org.springframework.util.StringUtils;
+
+import java.lang.annotation.Annotation;
+import java.util.Arrays;
+import java.util.Set;
+
+/**
+ * A {@link ClassPathBeanDefinitionScanner} that registers Mappers by {@code basePackage}, {@code annotationClass}, or
+ * {@code markerInterface}. If an {@code annotationClass} and/or {@code markerInterface} is specified, only the
+ * specified types will be searched (searching for all interfaces will be disabled).
+ * <p>
+ * This functionality was previously a private class of {@link MapperScannerConfigurer}, but was broken out in version
+ * 1.2.0.
+ *
+ * @author Hunter Presnall
+ * @author Eduardo Macarron
+ *
+ * @see MapperFactoryBean
+ * @since 1.2.0
+ */
+public class ClassPathMapperScanner extends ClassPathBeanDefinitionScanner {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(ClassPathMapperScanner.class);
+
+  private boolean addToConfig = true;
+
+  private boolean lazyInitialization;
+
+  private SqlSessionFactory sqlSessionFactory;
+
+  private SqlSessionTemplate sqlSessionTemplate;
+
+  private String sqlSessionTemplateBeanName;
+
+  private String sqlSessionFactoryBeanName;
+
+  private Class<? extends Annotation> annotationClass;
+
+  private Class<?> markerInterface;
+
+  private Class<? extends MapperFactoryBean> mapperFactoryBeanClass = MapperFactoryBean.class;
+
+  public ClassPathMapperScanner(BeanDefinitionRegistry registry) {
+    super(registry, false);
+  }
+
+  public void setAddToConfig(boolean addToConfig) {
+    this.addToConfig = addToConfig;
+  }
+
+  public void setAnnotationClass(Class<? extends Annotation> annotationClass) {
+    this.annotationClass = annotationClass;
+  }
+
+  /**
+   * Set whether enable lazy initialization for mapper bean.
+   * <p>
+   * Default is {@code false}.
+   * </p>
+   *
+   * @param lazyInitialization
+   *          Set the @{code true} to enable
+   * @since 2.0.2
+   */
+  public void setLazyInitialization(boolean lazyInitialization) {
+    this.lazyInitialization = lazyInitialization;
+  }
+
+  public void setMarkerInterface(Class<?> markerInterface) {
+    this.markerInterface = markerInterface;
+  }
+
+  public void setSqlSessionFactory(SqlSessionFactory sqlSessionFactory) {
+    this.sqlSessionFactory = sqlSessionFactory;
+  }
+
+  public void setSqlSessionTemplate(SqlSessionTemplate sqlSessionTemplate) {
+    this.sqlSessionTemplate = sqlSessionTemplate;
+  }
+
+  public void setSqlSessionTemplateBeanName(String sqlSessionTemplateBeanName) {
+    this.sqlSessionTemplateBeanName = sqlSessionTemplateBeanName;
+  }
+
+  public void setSqlSessionFactoryBeanName(String sqlSessionFactoryBeanName) {
+    this.sqlSessionFactoryBeanName = sqlSessionFactoryBeanName;
+  }
+
+  /**
+   * @deprecated Since 2.0.1, Please use the {@link #setMapperFactoryBeanClass(Class)}.
+   */
+  @Deprecated
+  public void setMapperFactoryBean(MapperFactoryBean<?> mapperFactoryBean) {
+    this.mapperFactoryBeanClass = mapperFactoryBean == null ? MapperFactoryBean.class : mapperFactoryBean.getClass();
+  }
+
+  /**
+   * Set the {@code MapperFactoryBean} class.
+   *
+   * @param mapperFactoryBeanClass
+   *          the {@code MapperFactoryBean} class
+   * @since 2.0.1
+   */
+  public void setMapperFactoryBeanClass(Class<? extends MapperFactoryBean> mapperFactoryBeanClass) {
+    this.mapperFactoryBeanClass = mapperFactoryBeanClass == null ? MapperFactoryBean.class : mapperFactoryBeanClass;
+  }
+
+  /**
+   * 配置父扫描程序以搜索正确的界面。它可以搜索所有接口或仅搜索那些
+   * 扩展了markerInterface或/和那些用notificationClass注释的标记
+   */
+  public void registerFilters() {
+    boolean acceptAllInterfaces = true;
+
+    // 如果指定，请使用给定的注释和/或标记界面
+    if (this.annotationClass != null) {
+      addIncludeFilter(new AnnotationTypeFilter(this.annotationClass));
+      acceptAllInterfaces = false;
+    }
+
+    // 覆盖AssignableTypeFilter以忽略实际标记界面上的匹配
+    if (this.markerInterface != null) {
+      addIncludeFilter(new AssignableTypeFilter(this.markerInterface) {
+        @Override
+        protected boolean matchClassName(String className) {
+          return false;
+        }
+      });
+      acceptAllInterfaces = false;
+    }
+
+    if (acceptAllInterfaces) {
+      // 默认包含所有类的过滤器
+      addIncludeFilter((metadataReader, metadataReaderFactory) -> true);
+    }
+
+    // 排除package-info.java
+    addExcludeFilter((metadataReader, metadataReaderFactory) -> {
+      String className = metadataReader.getClassMetadata().getClassName();
+      return className.endsWith("package-info");
+    });
+  }
+
+  /**
+   * 调用父级搜索，该搜索将搜索并注册所有候选者。然后注册的对象处理以将它们设置为MapperFactoryBeans
+   * @param basePackages 要扫描的包路径
+   * @return 对应的BeanDefinition的包装类
+   */
+  @Override
+  public Set<BeanDefinitionHolder> doScan(String... basePackages) {
+    Set<BeanDefinitionHolder> beanDefinitions = super.doScan(basePackages);
+
+    if (beanDefinitions.isEmpty()) {
+      LOGGER.warn(() -> "No MyBatis mapper was found in '" + Arrays.toString(basePackages)
+          + "' package. Please check your configuration.");
+    } else {
+      processBeanDefinitions(beanDefinitions);
+    }
+
+    return beanDefinitions;
+  }
+
+  /**
+   * 给扫描到的处理器设置一些自定义的属性
+   * @param beanDefinitions 对应接口的 beanDefinition
+   */
+  private void processBeanDefinitions(Set<BeanDefinitionHolder> beanDefinitions) {
+    GenericBeanDefinition definition;
+    for (BeanDefinitionHolder holder : beanDefinitions) {
+      definition = (GenericBeanDefinition) holder.getBeanDefinition();
+      String beanClassName = definition.getBeanClassName();
+      LOGGER.debug(() -> "Creating MapperFactoryBean with name '" + holder.getBeanName() + "' and '" + beanClassName
+          + "' mapperInterface");
+
+      // 映射器接口是Bean的原始类但是，bean的实际类是MapperFactoryBean
+      //这里传入的是对应接口的全限定名，未来注入到 mapperFactoryBean中后，会被自动的转换成class
+      definition.getConstructorArgumentValues().addGenericArgumentValue(beanClassName);
+      //设置对应的class，细心点你会发现，他注入的属性并不是对应的接口，而是一个 MapperFactoryBean.class
+      definition.setBeanClass(this.mapperFactoryBeanClass);
+
+      definition.getPropertyValues().add("addToConfig", this.addToConfig);
+
+      boolean explicitFactoryUsed = false;
+      if (StringUtils.hasText(this.sqlSessionFactoryBeanName)) {
+        definition.getPropertyValues().add("sqlSessionFactory",
+            new RuntimeBeanReference(this.sqlSessionFactoryBeanName));
+        explicitFactoryUsed = true;
+      } else if (this.sqlSessionFactory != null) {
+        definition.getPropertyValues().add("sqlSessionFactory", this.sqlSessionFactory);
+        explicitFactoryUsed = true;
+      }
+
+      if (StringUtils.hasText(this.sqlSessionTemplateBeanName)) {
+        if (explicitFactoryUsed) {
+          LOGGER.warn(
+              () -> "Cannot use both: sqlSessionTemplate and sqlSessionFactory together. sqlSessionFactory is ignored.");
+        }
+        definition.getPropertyValues().add("sqlSessionTemplate",
+            new RuntimeBeanReference(this.sqlSessionTemplateBeanName));
+        explicitFactoryUsed = true;
+      } else if (this.sqlSessionTemplate != null) {
+        if (explicitFactoryUsed) {
+          LOGGER.warn(
+              () -> "Cannot use both: sqlSessionTemplate and sqlSessionFactory together. sqlSessionFactory is ignored.");
+        }
+        definition.getPropertyValues().add("sqlSessionTemplate", this.sqlSessionTemplate);
+        explicitFactoryUsed = true;
+      }
+
+      if (!explicitFactoryUsed) {
+        LOGGER.debug(() -> "Enabling autowire by type for MapperFactoryBean with name '" + holder.getBeanName() + "'.");
+        definition.setAutowireMode(AbstractBeanDefinition.AUTOWIRE_BY_TYPE);
+      }
+      definition.setLazyInit(lazyInitialization);
+    }
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  protected boolean isCandidateComponent(AnnotatedBeanDefinition beanDefinition) {
+    return beanDefinition.getMetadata().isInterface() && beanDefinition.getMetadata().isIndependent();
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  protected boolean checkCandidate(String beanName, BeanDefinition beanDefinition) {
+    if (super.checkCandidate(beanName, beanDefinition)) {
+      return true;
+    } else {
+      LOGGER.warn(() -> "Skipping MapperFactoryBean with name '" + beanName + "' and '"
+          + beanDefinition.getBeanClassName() + "' mapperInterface" + ". Bean already defined with the same name!");
+      return false;
+    }
+  }
+
+}
